e051c6f291b71c4db60b3034dbf5c271bee66331
zhaoqing <alex.q.chao@gmail.com>
2015-02-27 15:23:04 +0800

    add a sample: qpm_simple_daemon.php

---
+++ examples/qpm_simple_daemon.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__.'/bootstrap.inc.php';
+require_once 'qpm/process/Process.php';
+use qpm\process\Process as Process;
+
+//实际的工作内容
+function work() {
+  while(true) {
+    file_put_contents(__FILE__'.log', date('Y-m-d H:i:s')."\n", FILE_APPEND);
+    sleep(10);
+   };
+}
+
+//通过回调启动子进程
+Process::current()->forkByCallable(
+  function() {
+    //子进程将自己转入后台
+    Process::current()->toBackground();
+    work();
+  }
+);
