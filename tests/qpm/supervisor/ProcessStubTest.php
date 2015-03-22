fc9cf8818328557e074b102cf225a82ef51de131
zhaoqing <alex.q.chao@gmail.com>
2015-03-22 20:43:45 +0800

    "added a testcase"

---
+++ tests/qpm/supervisor/ProcessStubTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace qpm\supervisor;
+
+class ProcessStubTest extends \PHPUnit_Framework_TestCase 
+
+{
+    
+    public function testIsTimeout() {
+        $this->markTestIncomplete();
+    }
+}
